9862d02d0bcbe5aeeb49b23cd6099b2020e2d66b
Andrej Mihalylovics <andrej.mihalylovics@gmail.com>
2021-02-08 20:49:45 +0000

    mvp done

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -2,11 +2,17 @@ public class WaterBottle {
 
     private int volume;
 
-    public WaterBottle(int volume){
+    public WaterBottle (){
         this.volume = 100;
     }
 
-    public int drink(){
-        return volume - 10;
-    }
+    public int getVolume(){return this.volume;}
+
+    public void drink(){this.volume -= 10;}
+
+    public void empty(){this.volume = 0;}
+
+    public void fill() {this.volume = 200;}
+
+
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+
+
+    WaterBottle waterbottle;
+
+
+    @Before
+
+    public void before() {
+        waterbottle = new WaterBottle();
+    }
+
+    @Test
+
+    public void hasVolume() {
+        assertEquals(100, waterbottle.getVolume());
+    }
+    @Test
+
+    public void drinkTest() {
+        waterbottle.drink();
+        assertEquals(90, waterbottle.getVolume());
+    }
+    @Test
+
+    public void emptyTest() {
+        waterbottle.empty();
+        assertEquals(0, waterbottle.getVolume());
+    }
+
+    @Test
+
+    public void fillTest() {
+        waterbottle.fill();
+        assertEquals(200, waterbottle.getVolume());
+    }
 }
